912d3f0c3a8a0f3df91149eaed2892610c73d5e2
Govindha raju <kgrgraju@gmail.com>
2023-06-05 02:09:15 +1000

    version 0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-import { Layout, Button, Card, Carousel, Image, Input, Space } from "antd";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { useRef, useState } from "react";
+import {
+  Layout,
+  Button,
+  Card,
+  Carousel,
+  Image,
+  Input,
+  Space,
+  Form,
+  message,
+} from "antd";
 const { Header, Content } = Layout;
+import type { FormInstance } from "antd/es/form";
+import axios from "axios";
 import logo from "./assets/logo.png";
 import frame from "./assets/frame.svg";
 import abstraction from "./assets/abstraction.png";
@@ -13,16 +25,151 @@ import offerTwo from "./assets/offerTwo.png";
 import offerThree from "./assets/offerThree.png";
 import In from "./assets/in.svg";
 import logoWhite from "./assets/logoWhite.svg";
+import shiv from "./assets/advisors/shiv.jpg";
+import mike from "./assets/advisors/mike.jpg";
+import arun from "./assets/advisors/arun.jpeg";
+
 import "./App.css";
 
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 24 },
+};
+
+const advisorsList = [
+  {
+    name: "SHIV KUMAR",
+    img: shiv,
+    in: "https://www.linkedin.com/in/shiv-kumar-ramani-149a70205/",
+  },
+  {
+    name: "Mike Gault",
+    img: mike,
+    in: "https://www.linkedin.com/in/mikegault1/",
+  },
+  {
+    name: "Arun Kothanath",
+    img: arun,
+    in: "https://www.linkedin.com/in/arunk/",
+  },
+];
+const baseUrl = "https://api.brevo.com/v3/smtp";
+
 function App() {
+  const [successOne, setSuccessOne] = useState(false as boolean);
+  const [successTwo, setSuccessTwo] = useState(false as boolean);
+  const formOneRef = useRef<FormInstance>(null);
+  const formTwoRef = useRef<FormInstance>(null);
+  const inputRef = useRef(null);
+
+  const onSubscribe = async (values: any) => {
+    const { email = "" } = values;
+    const emailConfig: any = {
+      sender: {
+        name: "Chainize",
+        email: "ontact@chainize.io",
+      },
+      to: [
+        {
+          email: "ontact@chainize.io",
+          name: "Chainize",
+        },
+      ],
+      subject: "Subscribe to our Newsletter",
+      htmlContent: `<html><head></head><body><p>Hello, <table><tr><td>Email:</td><td>${email}</td></tr></table></body></html>`,
+    };
+    await axiosCall(emailConfig);
+    setSuccessTwo(true);
+    formTwoRef.current?.resetFields();
+  };
+
+  const getAdvisors = () =>
+    advisorsList.map((list: any) => (
+      <div key={list?.in}>
+        <Image width={200} preview={false} src={list?.img} />
+        <h4>{list?.name}</h4>
+        <a rel="noopener" href={list?.in} target="_blank">
+          <img src={In} alt="LinkedIn" className="in" />
+        </a>
+      </div>
+    ));
+  const axiosInstance: any = axios.create({
+    baseURL: baseUrl,
+    headers: {
+      accept: "application/json",
+      "api-key":
+        "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",
+      "content-type": "application/json",
+    },
+  });
+
+  const axiosCall = async (requestConfig: any) => {
+    try {
+      await axiosInstance.post("/email", {
+        ...requestConfig,
+      });
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  // clean useAxios
+  const onFinish = async (values: any) => {
+    const { name = "", phone = "", email = "", company = "" } = values;
+    //contact@chainize.io
+    //kgrgraju@gmail.com
+    const emailConfig: any = {
+      sender: {
+        name,
+        email: "ontact@chainize.io",
+      },
+      to: [
+        {
+          email: "ontact@chainize.io",
+          name: "Chainize",
+        },
+      ],
+      subject: "GET IN TOUCH",
+      htmlContent: `<html><head></head><body><p>Hello, <table><tr><td>Name:</td><td>${name}</td></tr><tr><td>Phone Number:</td><td>${phone}</td></tr><tr><td>Email:</td><td>${email}</td></tr><tr><td>Company Name:</td><td>${company}</td></tr></table></body></html>`,
+    };
+    await axiosCall(emailConfig);
+    setSuccessOne(true);
+    formOneRef.current?.resetFields();
+  };
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+    },
+  };
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }: any) => (
+    <button
+      {...props}
+      className="slick-prev slick-arrow"
+      aria-hidden="true"
+      aria-disabled={currentSlide === 0 ? true : false}
+      type="button"
+    ></button>
+  );
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }: any) => (
+    <button
+      {...props}
+      className="slick-next slick-arrow"
+      aria-hidden="true"
+      aria-disabled={currentSlide === slideCount - 1 ? true : false}
+      type="button"
+    ></button>
+  );
+  const year = new Date().getFullYear();
   return (
     <Layout className="site-layout">
       <Header className="header">
-        <img src={logo} alt="logo" className="logo" />
+        <a href="#home">
+          <img src={logo} alt="logo" className="logo" />
+        </a>
       </Header>
       <Content style={{ overflow: "initial" }}>
-        <section>
+        <section id="home">
           <div className="row home">
             <div className="home-left">
               <h1>Simplify blockchain Technology for Enterprises</h1>
@@ -32,9 +179,11 @@ function App() {
                 Focus on growing your business with our easy to use adapters for
                 different industries
               </p>
-              <Button type="primary" className="btn" size="large">
-                Get in Touch
-              </Button>
+              <a href="#getInTouch">
+                <Button type="primary" className="btn" size="large">
+                  Get in Touch
+                </Button>
+              </a>
             </div>
             <div className="home-right">
               <img src={frame} alt="frame" />
@@ -97,7 +246,6 @@ function App() {
                   data, it is easy to grow and adapt quickly.
                 </p>
               </Card>
-
               <Card
                 className="benefits-card"
                 cover={<img alt="market" className="img" src={market} />}
@@ -116,10 +264,10 @@ function App() {
           <div className="offerings">
             <h1>Our Offerings</h1>
             <Carousel
-              autoplay
+              // autoplay
               arrows={true}
-              prevArrow={<LeftOutlined />}
-              nextArrow={<RightOutlined />}
+              prevArrow={<SlickArrowLeft />}
+              nextArrow={<SlickArrowRight />}
             >
               <div>
                 <div className="group-inner">
@@ -194,39 +342,65 @@ function App() {
             </Carousel>
           </div>
         </section>
-        <section className="gray-section">
+        <section className="gray-section advisors-section">
           <div className="advisors">
             <h1>Advisors</h1>
-            <div className="advisors-list">
+            <div className="advisors-list">{getAdvisors()}</div>
+          </div>
+        </section>
+        <section className="get-in-touch-section" id="getInTouch">
+          <div className="get-in-touch">
+            <h1>Get in touch</h1>
+            <Form
+              {...layout}
+              name="nest-messages"
+              onFinish={onFinish}
+              style={{ width: 400 }}
+              validateMessages={validateMessages}
+              autoComplete="off"
+              ref={formOneRef}
+            >
+              <Form.Item
+                name={["name"]}
+                label="Name"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Enter the name" />
+              </Form.Item>
+              <Form.Item
+                name={["phone"]}
+                label="Phone Number"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Enter the phone number" />
+              </Form.Item>
+
+              <Form.Item
+                name={["email"]}
+                label="Email"
+                rules={[{ type: "email" }, { required: true }]}
+              >
+                <Input placeholder="Enter the email" />
+              </Form.Item>
+              <Form.Item
+                name={["company"]}
+                label="Company Name"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Enter the company name" />
+              </Form.Item>
               <div>
-                <Image
-                  width={200}
-                  preview={false}
-                  src="https://i.pinimg.com/564x/b6/5e/69/b65e69dadf225e338c2dfc9511ed9704.jpg"
-                />
-                <h4>SHIV KUMAR</h4>
-                <img src={In} alt="LinkedIn" className="in" />
+                <Button type="primary" className="btn" htmlType="submit">
+                  Submit
+                </Button>
               </div>
 
-              <div>
-                <Image
-                  width={200}
-                  preview={false}
-                  src="https://i.pinimg.com/564x/b6/5e/69/b65e69dadf225e338c2dfc9511ed9704.jpg"
-                />
-                <h4>Mike Gault</h4>
-                <img src={In} alt="LinkedIn" className="in" />
-              </div>
-              <div>
-                <Image
-                  width={200}
-                  preview={false}
-                  src="https://i.pinimg.com/564x/b6/5e/69/b65e69dadf225e338c2dfc9511ed9704.jpg"
-                />
-                <h4>Arun Kothanath</h4>
-                <img src={In} alt="LinkedIn" className="in" />
-              </div>
-            </div>
+              {successOne && (
+                <div className="success-msg">
+                  Thanks for contacting us. We will get back to you soon.
+                </div>
+              )}
+            </Form>
           </div>
         </section>
         <footer>
@@ -247,20 +421,50 @@ function App() {
             <div className="subscribe-info">
               <h4>Subscribe to our Newsletter</h4>
               <Space.Compact style={{ width: "100%" }}>
-                <Input placeholder="Enter your email address" />
-                <Button type="primary">Subscribe</Button>
+                <Form
+                  name="customized_form_controls"
+                  layout="inline"
+                  onFinish={onSubscribe}
+                  style={{ width: 415 }}
+                  validateMessages={validateMessages}
+                  labelCol={{ span: 24 }}
+                  wrapperCol={{ span: 24 }}
+                  autoComplete="off"
+                  ref={formTwoRef}
+                >
+                  <Form.Item
+                    name={["email"]}
+                    rules={[{ type: "email" }, { required: true }]}
+                  >
+                    <Input
+                      placeholder="Enter your email address"
+                      ref={inputRef}
+                    />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      Subscribe
+                    </Button>
+                  </Form.Item>
+                  {successTwo && (
+                <div className="success-msg">
+                  Thanks for Subscribing to the Newsletter.
+                </div>
+              )}
+                </Form>
+               
               </Space.Compact>
-              <img src={In} alt="LinkedIn" className="in" />
+              {/* <img src={In} alt="LinkedIn" className="in" /> */}
             </div>
           </div>
           <div className="footer-flex">
-            <span>Contact Us</span>
+            {/* <span>Contact Us</span>
             <span>Be part of our team</span>
             <span>Terms of use</span>
             <span>Privacy Policy</span>
-            <span>Cookies Policy</span>
+            <span>Cookies Policy</span> */}
           </div>
-          <span> © 2023 www.chainize.com. All rights reserved. </span>
+          <span> © {year} www.chainize.com. All rights reserved. </span>
         </footer>
       </Content>
     </Layout>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: "public/index.html", dest: "dist" },
+        { src: "public/assets", dest: "dist" },
+      ],
+      flatten: false,
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 500,
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) { 
+            return 'vendor';
+          } 
+          return 'app';
+        },
+      },
+    },
+  },
+});
